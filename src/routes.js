bc72c9148e060dabd8c606a503ecb625d22d76d7
nicknamedelta <evaldo.felipe2@gmail.com>
2020-03-23 16:03:34 -0300

    2step - addresses association, belongsTo, hasMany

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const UserController = require("./controllers/UserController");
+const AdressController = require("./controllers/AddressController");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({
-    response: "Hello Dev!"
-  });
-});
-
 // list all users
-routes.get("/users", (req, res) => {
-  UserController.index;
-});
-
+routes.get("/users", UserController.index);
 // create new user
-routes.post("/users", (req, res) => {
-  UserController.store;
-});
+routes.post("/users", UserController.store);
+
+// list all user addreses
+routes.get("user/:user_id/addresses", AdressController.index);
+// create new address
+routes.post("user/:user_id/addresses", AdressController.store);
 
 module.exports = routes;
